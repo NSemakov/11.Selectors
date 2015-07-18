4b8d7f515f0e89b285bf9a8329ae63dd3d356efd
NSemakov <nsemakov@yahoo.com>
2015-07-18 23:28:32 +0400

    Initial Commit

---
+++ 11. Selectors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  11. Selectors
+//
+//  Created by Admin on 18.07.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
